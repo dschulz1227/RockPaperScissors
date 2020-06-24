a4c85b227f3d18fa536c14aa3ab7be58df6132be
Damon Schulz <damon.schulz@gmail.com>
2020-06-23 23:15:27 -0500

    Added Game and Player-->Computer classes. Added functions for user to select gesture. added method for computer to randomize gesutre

--- RPSLS.js
+++ RPSLS.js
@@ -15,31 +15,65 @@ class Game{
     constructor(){
         this.playerOne = new Player ("Damon");
         this.playerTwo = new Player ("Steve");
-
-        this.gesture = [];
-
-        this.gesture.push(new Gestures(rock));
-        this.gesture.push(new Gestures(paper));
-        this.gesture.push(new Gestures(scissors));
-        this.gesture.push(new Gestures(lizard));
-        this.gesture.push(new Gestures(spock));
-
-        
     }
+    runGame(){   // main method
+        this.displayRules;
+        this.playerOne.chooseGesture();
+        this.playerTwo.chooseGesture();
+        this.rules = "Each player will choose a gesture, whomever's gesture is dominant will win the round. Whoever wins 2 out of 3 rounds is the ultimate RPSLP champion"
+        
+    findWinner(){
+        if(this.playerOne.chooseGesture) === (this.playerTwo.chooseGesture){
+            console.log("It's a tie!")
+        }
+        else if {this.playerOne.chooseGesture === 'Rock' && this.playerTwo.chooseGesture === 'Scissors'){
+            // console.log("Player One wins this round!")
+            displayWinner()
+            }
+        }   
+    }
+
+
+    }
+    displayWinner(playerOne.userInput, playerTwo.userInput){
+       if(this.playerOne.chooseGesture > this.playerTwo,chooseGesture){
+
+       }
+
+    displayRules() {
+        console.log("these are the rules!");
+    }
+        
 }
+
 
 
 class Player{
-    constructor(name){
+    constructor(name, userInput){
         this.score = 0;
         this.name = name;
+        this.gestureChoice = userInput;
+        this.gestures = ['rock', 'paper', 'scissors', 'lizard', 'Spock'];
+    }
+    chooseGesture(){
+        let userInput = prompt("Which gesture would you like to use?".  "Rock, Paper, Scissors, Lizard, or Spock")
+        return userInput;
+    }
+
+}
+
+
+class Computer extends Player {
+    constructior(){
+        let computerSelection = gestures[Math.floor(Math.random()) * this.gestures.length]
+        console.log(computerSelection)
     }
 }
 
 
-class Gestures{
-    constructor(){
+
+
+
         
 
-    }
-}
+
